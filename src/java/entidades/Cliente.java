5b3748baf01dc93272d0b526efd02a268e76d9c6
mikelmatilla <mikel.matilla1@gmail.com>
2021-12-02 12:07:49 +0100

    Entidad cliente con javadoc

--- src/java/entidades/Cliente.java
+++ src/java/entidades/Cliente.java
@@ -7,40 +7,72 @@ package entidades;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
-import java.util.Set;
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
 /**
- *
+ * Clase de la entidad Cliente
  * @author Mikel Matilla
  */
 @Entity
 @Table(name="cliente", schema="gesredb")
 public class Cliente extends User implements Serializable {
     
-    //Atributos
+    /**
+     * Fecha de regitro del cliente
+     */
     @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
     private Timestamp fechaRegistro;
-    private Set<Incidencia> incidencias;
-
-    //Getters
+    
+    /**
+     * Metodo getter de la fecha de registro
+     * @return feecha de registro
+     */
     public Timestamp getFechaRegistro() {
         return fechaRegistro;
     }
 
-    public Set<Incidencia> getIncidencias() {
-        return incidencias;
-    }
-
-    //Setters
+    /**
+     * Metodo setter de la fecha de registro
+     * @param fechaRegistro Fecha de registro
+     */
     public void setFechaRegistro(Timestamp fechaRegistro) {
         this.fechaRegistro = fechaRegistro;
     }
 
-    public void setIncidencias(Set<Incidencia> incidencias) {
-        this.incidencias = incidencias;
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 73 * hash + Objects.hashCode(this.fechaRegistro);
+        return hash;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.fechaRegistro, other.fechaRegistro)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "fechaRegistro=" + fechaRegistro + '}';
+    }
+    
 }
